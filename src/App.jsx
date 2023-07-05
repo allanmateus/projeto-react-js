89ae419ae2f771de89cf4694a1a7d0509214a330
allanmateus <allanpmateus@gmail.com>
2023-07-05 09:18:17 -0300

    Configurações iniciais

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import './components/HelloWorld'
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <>
+      <HelloWorld/>
+      <div>
+        <h1>Allan</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Frase.jsx
@@ -0,0 +1,11 @@
+function Frase(){
+    return(
+        <>
+            <div>
+                <h1>Frase...</h1>
+            </div>
+        </>
+    )
+}
+
+export default Frase
